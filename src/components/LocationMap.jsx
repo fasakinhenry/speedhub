5bd4090285f7f09937a0ad6b08691252c9ffe31a
Fasakin Henry <fasakinhenry@gmail.com>
2025-01-15 13:42:41 +0100

    feat: add the location map component

---
+++ src/components/LocationMap.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const LocationMap = ({ serverInfo }) => {
+  const position = [serverInfo.latitude, serverInfo.longitude];
+
+  return (
+    <div className="bg-white p-6 rounded-lg shadow-lg">
+      <h3 className="text-lg font-semibold mb-4">Server Location</h3>
+      <div className="h-[400px]">
+        <MapContainer center={position} zoom={13} className="h-full">
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={position}>
+            <Popup>
+              Server Location: {serverInfo.location}
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+    </div>
+  );
+};
+
+export default LocationMap;
