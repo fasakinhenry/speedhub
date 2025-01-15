24a220a02ea40f83090894f0b734aff089c3420e
Fasakin Henry <fasakinhenry@gmail.com>
2025-01-15 13:43:09 +0100

    feat: add the location map component

--- src/components/LocationMap.jsx
+++ src/components/LocationMap.jsx
@@ -6,18 +6,16 @@ const LocationMap = ({ serverInfo }) => {
   const position = [serverInfo.latitude, serverInfo.longitude];
 
   return (
-    <div className="bg-white p-6 rounded-lg shadow-lg">
-      <h3 className="text-lg font-semibold mb-4">Server Location</h3>
-      <div className="h-[400px]">
-        <MapContainer center={position} zoom={13} className="h-full">
+    <div className='bg-white p-6 rounded-lg shadow-lg'>
+      <h3 className='text-lg font-semibold mb-4'>Server Location</h3>
+      <div className='h-[400px]'>
+        <MapContainer center={position} zoom={13} className='h-full'>
           <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           />
           <Marker position={position}>
-            <Popup>
-              Server Location: {serverInfo.location}
-            </Popup>
+            <Popup>Server Location: {serverInfo.location}</Popup>
           </Marker>
         </MapContainer>
       </div>
