34f542ba2c9bdb332dbe2e6dfabe3658266c8368
Fasakin Henry <fasakinhenry@gmail.com>
2025-01-15 13:55:53 +0100

    feat: update the App.jsx to contain changes in Speedtest

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import SpeedTest from './components/SpeedTest';
+import SpeedTestComponent from './components/SpeedTest';
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className='min-h-screen bg-gray-50'>
       <Navbar />
-      <main className="container mx-auto px-4 py-8">
-        <SpeedTest />
+      <main className='container mx-auto px-4 py-8'>
+        <SpeedTestComponent />
       </main>
       <Footer />
     </div>
